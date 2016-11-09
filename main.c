5d83c3d9128ac1cd00b663cb2c0f6cba4e4454aa
Ben Saward <benjamin.saward@gmail.com>
2016-11-09 09:59:27 +0000

    working on reading header

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <project.h>
+#include <string.h>
+#include </home/ben/Documents/Oxford/DTP/project/DTC-Project/project.h>
 
 // BEGIN CONSTS
 const char *INPUTIMAGE="image-1.png";
@@ -16,9 +17,12 @@ int main (int argc, char **argv)
 	FILE *image = fopen(INPUTIMAGE, "r");
 	if (image == NULL)
 	{
-		fprintf("Could not open image.\n");
+		printf("Could not open image.\n");
 		return -1;
 	}
+	int dimensions[2];
+	getdimensions(image, dimensions);
+	printf("x = %d, y = %d", dimensions[0], dimensions[1]);
 	// READ FILE UNTIL WE FIND IDAT
 	return 0;
 }
--- project.h
+++ project.h
@@ -1,34 +1,61 @@
 #ifndef _PROJECT_H
 #define _PROJECT_H
 // we are dealing with images so lets struct our pixel format
-typedef struct pixel // use chars to save memory?
+typedef struct // use chars to save memory?
 {
 	int red;
 	int green;
 	int blue;
 	// include? int alpha;
-};
+} pixel;
 //BEGIN FUNCTION DEFS
 
-short int checkimagetype (FILE *image); // lets find the IDHR of our png file and read it to see if it matches the type we plan on reading
+void getdimensions(FILE *image, int *retvalue); // lets find the IDHR of our png file and read it to get dimensions
 void findwell(/* args */); // use circular edge detection to find the well plates
 void greyscaleimage(pixel **image); /* for this we need our image in grey scale */
 
 //END FUNCTION DEFS
 
-int checkimagetype (FILE *image) //return 1 if this matches what we expect, 0 otherwise
+void getdimensions(FILE *image, int *retvalue) //return x, y dimensions if this matches what we expect, NULL otherwise
 {
-	char *buffer=malloc(sizeof(char)*100); //how many bytes usually before IHDR? +13 bytes for IHDR +4 for "IHDR"
-	char *IDHR="IHDR";
+	char *buffer=malloc(sizeof(char)*101); //how many bytes usually before IHDR? +13 bytes for IHDR +4 for "IHDR"
+	strcpy(buffer, "00000000000000000000000000");
+	char *IHDR="IHDR";
 	int bytes_read=0;
-	while(!(strstr(buffer, IDHR) != NULL))
-	{
-		fgets(buffer, 40, image);
-	}
+	//while((strstr(buffer, IHDR) == NULL))
+	//{
+		fgets(buffer, 100, image);
+		//if ((strstr(buffer, IHDR) != NULL)
+       // {
+          //  break;
+      //  }
+		printf("%s\n", buffer);
+	//}
 	//lets make a string with our header in :D
-	char *header=strstr(buffer, IHDR);
-	header+=4; //move over IHDR into the buffer
-	*(header+14)='\0'; // null terminate so we have a complete header
+	char *tempheader=strstr(buffer, IHDR);
+	tempheader+=4; //move over IHDR into the buffer
+	// causes SIGSEV - trying to overwrite ROM *(header+14)='\0'; // null terminate so we have a complete header
+	char *header=malloc(97);
+	strcpy(header, tempheader);
+	header[13]='\0';
+	printf("%s\n", header);
+	int x,y;
+	int i=0;
+	scanf(header, "%d%d", &x, &y);
+	/*char *x_char=malloc(sizeof(char)*5);
+	x_char[4]='\0';
+	for (i=0; i<4; i++)
+    {
+        x_char[i]=header[i];
+    }
+    x=(int)x_char;
+    for (i=4;i<8; i++)
+    {
+        x_char[i]=header[i];
+    }
+    y=(int)x_char; */
+    retvalue[0]=x;
+    retvalue[1]=y;
 	// free(buffer); //can we call without breaking header?
 }
 
